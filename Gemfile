9ff42624d40c52243e209e533489a57e7d57ecee
Nathan Khuu <nkhuu@berkeley.edu>
2016-12-21 23:02:09 -0800

    working on using api

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 ruby '2.3.0'
+gem 'httparty'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 gem 'sqlite3'
 gem 'puma', '~> 3.0'
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,14 @@
 class Recipe < ApplicationRecord
 	has_and_belongs_to_many :recipes
 	has_and_belongs_to_many :users
+
+	include HTTParty
+	ENV["FOOD2FORK_KEY"] = '324016b51f0aeb6ba0bd10820ac8b167'
+	base_uri 'http://food2fork.com/api'
+	default_params key: ENV["FOOD2FORK_KEY"]
+	format :json
+
+	def self.for term
+		get("/search", query: { q: term})["recipes"]
+	end
 end
